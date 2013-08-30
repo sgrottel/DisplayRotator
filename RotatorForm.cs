03f83fbcfb56ad9ce99cea1795a6105c0267a641
sgrottel <none@none>
2013-08-30 23:27:48 +0200

    Icons and Images

--- RotatorForm.cs
+++ RotatorForm.cs
@@ -30,16 +30,99 @@ namespace DisplayRotator {
             this.rot180Button.Tag = 2;
             this.rot90cwButton.Tag = 3;
 
-            // TODO: Button Icons
-            Bitmap bmp = new Bitmap(80, 80, System.Drawing.Imaging.PixelFormat.Format32bppArgb);
-            using (Graphics g = Graphics.FromImage(bmp)) {
-                g.Clear(Color.Red);
-            }
-            this.doNothingButton.Image = bmp;
-            this.rot90ccwButton.Image = bmp;
-            this.rot180Button.Image = bmp;
-            this.rot90cwButton.Image = bmp;
+            DisplayOrientation ori = PInvoke.GetCurrentDisplayRotation();
 
+            //ori = DisplayOrientation.Horiz_0; // debugging!
+            //ori = DisplayOrientation.Vert_90;
+            //ori = DisplayOrientation.Horiz_180;
+            //ori = DisplayOrientation.Vert_270;
+
+            for (int i = 0; i < 4; i++) {
+                Bitmap bmp = new Bitmap(Properties.Resources.DisplayRotator_Device);
+                Bitmap dsk = new Bitmap(
+                    (((((int)ori) + i) % 2) == 1)
+                    ? Properties.Resources.DisplayRotator_DesktopVert
+                    : Properties.Resources.DisplayRotator_DesktopHoriz);
+                int xoff = 0;
+                int yoff = 0;
+                switch (((int)ori) * 4 + i) {
+                    case 0: break;
+                    case 1:
+                        dsk.RotateFlip(RotateFlipType.Rotate180FlipNone);
+                        yoff = -5;
+                        break;
+                    case 2:
+                        dsk.RotateFlip(RotateFlipType.Rotate180FlipNone);
+                        yoff = -5;
+                        break;
+                    case 3: break;
+                    case 4:
+                        bmp.RotateFlip(RotateFlipType.Rotate90FlipNone);
+                        dsk.RotateFlip(RotateFlipType.Rotate270FlipNone);
+                        xoff = 5;
+                        break;
+                    case 5:
+                        bmp.RotateFlip(RotateFlipType.Rotate90FlipNone);
+                        dsk.RotateFlip(RotateFlipType.Rotate270FlipNone);
+                        xoff = 5;
+                        break;
+                    case 6:
+                        bmp.RotateFlip(RotateFlipType.Rotate90FlipNone);
+                        dsk.RotateFlip(RotateFlipType.Rotate90FlipNone);
+                        break;
+                    case 7:
+                        bmp.RotateFlip(RotateFlipType.Rotate90FlipNone);
+                        dsk.RotateFlip(RotateFlipType.Rotate90FlipNone);
+                        break;
+                    case 8:
+                        bmp.RotateFlip(RotateFlipType.Rotate180FlipNone);
+                        yoff = 5;
+                        break;
+                    case 9:
+                        bmp.RotateFlip(RotateFlipType.Rotate180FlipNone);
+                        dsk.RotateFlip(RotateFlipType.Rotate180FlipNone);
+                        break;
+                    case 10:
+                        bmp.RotateFlip(RotateFlipType.Rotate180FlipNone);
+                        dsk.RotateFlip(RotateFlipType.Rotate180FlipNone);
+                        break;
+                    case 11:
+                        bmp.RotateFlip(RotateFlipType.Rotate180FlipNone);
+                        yoff = 5;
+                        break;
+                    case 12:
+                        bmp.RotateFlip(RotateFlipType.Rotate270FlipNone);
+                        dsk.RotateFlip(RotateFlipType.Rotate270FlipNone);
+                        //xoff = 5;
+                        break;
+                    case 13:
+                        bmp.RotateFlip(RotateFlipType.Rotate270FlipNone);
+                        dsk.RotateFlip(RotateFlipType.Rotate270FlipNone);
+                        //xoff = 5;
+                        break;
+                    case 14:
+                        bmp.RotateFlip(RotateFlipType.Rotate270FlipNone);
+                        dsk.RotateFlip(RotateFlipType.Rotate90FlipNone);
+                        xoff = -5;
+                        break;
+                    case 15:
+                        bmp.RotateFlip(RotateFlipType.Rotate270FlipNone);
+                        dsk.RotateFlip(RotateFlipType.Rotate90FlipNone);
+                        xoff = -5;
+                        break;
+                }
+
+                using (Graphics g = Graphics.FromImage(bmp)) {
+                    g.DrawImageUnscaled(dsk, xoff, yoff, 80, 80);
+                }
+                switch (i) {
+                    case 0: this.doNothingButton.Image = bmp; break;
+                    case 1: this.rot90ccwButton.Image = bmp; break;
+                    case 2: this.rot180Button.Image = bmp; break;
+                    case 3: this.rot90cwButton.Image = bmp; break;
+                    default: throw new Exception();
+                }
+            }
         }
 
         /// <summary>
